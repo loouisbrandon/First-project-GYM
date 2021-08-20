c70d9e2ee393fc21093a2a33fe021f0303bd5cf7
Loouisbrandon <brandon.martins@al.infnet.edu.br>
2021-08-20 12:28:55 -0300

    belezinha1

---
+++ belezinha/src/components/Header.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, IconButton, Toolbar, Collapse } from '@material-ui/core';
+import SortIcon from '@material-ui/icons/Sort';
+import TabScrollButton from '@material-ui/core/TabScrollButton';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    fontFamily: 'Nunito',
+  },
+  appbar: {
+    background: 'none',
+  },
+  appbarWrapper: {
+    width: '80%',
+    margin: '0 auto',
+  },
+  appbarTitle: {
+    flexGrow: '1',
+  },
+  icon: {
+    color: '#iii',
+    fontSize: '2rem',
+  },
+  colorText: {
+    color: '#808080',
+  },
+  container: {
+    textAlign: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: '4.5rem',
+  },
+  goDown: {
+    color: '#iii',
+    fontSize: '4rem',
+  },
+}));
+export default function Header() {
+  const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, []);
+  return (
+    <div className={classes.root} id="header">
+      <AppBar className={classes.appbar} elevation={0}>
+        <Toolbar className={classes.appbarWrapper}>
+          <h1 className={classes.appbarTitle}>
+            <span className={classes.colorText}>Pelos poderes de Grayskull.</span>
+          </h1>
+         
+         <IconButton>
+            <SortIcon className={classes.icon} />
+            <a>ovômorre</a>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1000 } : {})}
+        collapsedHeight={50}
+      >
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            Welcome to <br />
+            <span className={classes.colorText}>Paradise</span>
+          </h1>
+         <TabScrollButton to="place-to-visit" smooth={true}>
+            <IconButton>
+            <a>Descubra-se</a>
+             
+            </IconButton>
+          </TabScrollButton>
+        </div>
+      </Collapse>
+    </div>
+  );
+}
---
+++ belezinha/src/static/places.js
@@ -0,0 +1,18 @@
+const places = [
+  {
+    title: 'Ilha Grande',
+    description:
+      "Um paraíso localizado na Costa Verde do Rio de Janeiro, Ilha Grande é o destino perfeito para feriados e viagens de períodos curtos, principalmente para os cariocas e paulistas. Para os mais aventureiros, que encaram qualquer trilha, Ilha Grande é uma pedida excelente com trilhas para todos os níveis. Para quem quer relaxar, a Ilha também é incrível – areias brancas e águas cristalinas repletas de vida marinha!",
+    imageUrl: process.env.PUBLIC_URL + '/assets/praia-do-aventureiro-03.jpg',
+    time: 1500,
+  },
+  {
+    title: 'Saquarema',
+    description:
+      'Cidade predominantemente turística, é conhecida também como "A Capital Nacional do Surfe". As ondas de suas praias estão entre as melhores do país. Além dos campeonatos de surfe nacionais e internacionais, as festas religiosas constituem um importante atrativo para o turismo local.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/1_saquarema_principal-21497140.jpg',
+    time: 1500,
+  },
+];
+
+export default places;
