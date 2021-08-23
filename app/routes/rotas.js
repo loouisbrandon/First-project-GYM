2fc3dbcb3052cb3a6cc109a23db8f6df60a8a57b
Brandon Louis <Usurio@pc003.pms.rj.gov.br>
2021-08-23 17:16:48 -0300

    segurança

--- app/routes/rotas.js
+++ app/routes/rotas.js
@@ -4,9 +4,10 @@ const v1Sobre = require('./v1/sobre');
 const v1Cadastro = require('./v1/Cadastro');
 
 
+
 module.exports = (app, rota) => {
 
-   
+    
     v1Home(rota);
     v1Sobre(rota);
     v1Cadastro(rota);
